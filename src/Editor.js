65d153858ca62be33d01a77d2c441e83cbc60e59
Vijay Kumar <38584330+vijay48194@users.noreply.github.com>
2018-07-18 09:15:36 +0530

    Update Editor.js

--- src/Editor.js
+++ src/Editor.js
@@ -32,12 +32,6 @@ handleChange(data){
   render() {
     return (
       <div>
-      	
-      <nav>
-          <div className="nav-wrapper blue darken-2">
-            <a href="#" className="brand-logo center">Collab_Editor</a>
-          </div>
-        </nav>
      	  <AceEditor
 		    mode="javascript"
 		    theme="github"
@@ -46,23 +40,6 @@ handleChange(data){
 		    name="UNIQUE_ID_OF_DIV"
 		    editorProps={{$blockScrolling: true}}
 		  />
-		 <footer className="page-footer blue lighten-1 ">
-          <div className="container ">
-            <div className="row ">
-              <div className="col l6 s12">
-                <h5 className="white-text">Collab_Editor</h5>
-                <p className="grey-text text-lighten-4 ">Collab_Editor is a online live code editor</p>
-              </div>
-            
-            </div>
-          </div>
-          <div className="footer-copyright blue darken-3 ">
-            <div className="container">
-            Collab_Editor
-            © 2018 Copyright 
-            </div>
-          </div>
-        </footer>
       </div>
     );
   }
